f42cbde5fa80f29614e0049977b6f22d4dcb97d8
MinoruSekine <55676243+MinoruSekine@users.noreply.github.com>
2019-10-06 12:16:39 +0900

    GitHub Actions for CI.

---
+++ .github/workflows/install-elisp-lint.el
@@ -0,0 +1,10 @@
+(defun install-elisp-lint-batch ()
+  "Batch install elisp-lint on the command line."
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+  (package-initialize)
+  (when (not package-archive-contents)
+    (package-refresh-contents))
+  (package-install 'elisp-lint))
