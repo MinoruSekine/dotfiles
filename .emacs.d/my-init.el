5fc741c0f24fa544f0b76661f63ead0e967ff780
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2021-02-02 22:51:56 +0900

    Highlight too long line (over 80 chars) in C++ mode.
    It is sometimes difficult to shorten the line in C,
    because we avoid symbols' name conflictions without namespace.
    So I don't apply this in C mode now.

--- .emacs.d/my-init.el
+++ .emacs.d/my-init.el
@@ -241,6 +241,8 @@
   (add-to-list 'auto-mode-alist '("\\.h\\'" . c-mode))
   (add-to-list 'auto-mode-alist '("\\.mm\\'" . objc-mode))
   (font-lock-add-keywords 'c++-mode
+                          '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face prepend)))
+  (font-lock-add-keywords 'c++-mode
                           '(("[ \t]+$" . 'trailing-whitespace)))
   (font-lock-add-keywords 'c-mode
                           '(("[ \t]+$" . 'trailing-whitespace)))
