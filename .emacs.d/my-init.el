534c09970bc736206a30d2dc0e2e67f1e9c770ca
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2025-11-10 11:43:17 +0900

    fix: Move local defun to global. (#307)

--- .emacs.d/my-init.el
+++ .emacs.d/my-init.el
@@ -538,28 +538,29 @@ This function works if interval expired, interactive, and network available."
     (run-hooks 'my-after-ede-setup-hook))
   )
 
+(defun my-set-indent-tabs-mode ()
+  "Disable INDENT-TABS-MODE when .editorconfig is unavailable (PROPS is nil)
+and existing file includes no hard tab."
+  (defsubst my-buffer-contains-hard-tab-p (bytes-to-check)
+    "Return first BYTES-TO-CHECK bytes of current buffer
+ containing hard tab or not."
+    (save-restriction
+      (widen)
+      (save-excursion
+        (goto-char (point-min))
+        (let ((limit (min (point-max) bytes-to-check)))
+          (re-search-forward "\t" limit t)))))
+  (let* ((my-hard-tab-check-bytes 16384))
+    (when (and buffer-file-name
+               (not (and (fboundp 'editorconfig-core-get-properties)
+                         (editorconfig-core-get-properties)))
+               (or (not (file-exists-p buffer-file-name))
+                   (not (my-buffer-contains-hard-tab-p
+                         my-hard-tab-check-bytes))))
+      (setq indent-tabs-mode nil))))
+
 (defun my-indent-tabs-mode-for-file-without-hard-tab ()
   "Set INDENT-TABS-MODE as nil for files without hard tab."
-  (defun my-set-indent-tabs-mode ()
-    "Disable INDENT-TABS-MODE when .editorconfig is unavailable (PROPS is nil)
- and existing file includes no hard tab."
-    (defsubst my-buffer-contains-hard-tab-p (bytes-to-check)
-      "Return first BYTES-TO-CHECK bytes of current buffer
- containing hard tab or not."
-      (save-restriction
-        (widen)
-        (save-excursion
-          (goto-char (point-min))
-          (let ((limit (min (point-max) bytes-to-check)))
-            (re-search-forward "\t" limit t)))))
-    (let* ((my-hard-tab-check-bytes 16384))
-      (when (and buffer-file-name
-                 (not (and (fboundp 'editorconfig-core-get-properties)
-                           (editorconfig-core-get-properties)))
-                 (or (not (file-exists-p buffer-file-name))
-                     (not (my-buffer-contains-hard-tab-p
-                           my-hard-tab-check-bytes))))
-        (setq indent-tabs-mode nil))))
   (add-hook 'find-file-hook #'my-set-indent-tabs-mode)
   (add-hook 'after-change-major-mode-hook #'my-set-indent-tabs-mode))
 
